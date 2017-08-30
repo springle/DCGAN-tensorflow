6d8b9e388e1cc78b3148ee924d5616028ab61b46
Sam Pringle <15springle@gmail.com>
2017-08-30 11:57:40 -0700

    Change main.py args

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ flags.DEFINE_boolean("crop", False, "True for training, False for testing [False
 flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
 FLAGS = flags.FLAGS
 
-def main(_):
+def main(server, log_dir, context):
   pp.pprint(flags.FLAGS.__flags)
 
   if FLAGS.input_width is None:
@@ -34,16 +34,14 @@ def main(_):
   if FLAGS.output_width is None:
     FLAGS.output_width = FLAGS.output_height
 
-  if not os.path.exists(FLAGS.checkpoint_dir):
-    os.makedirs(FLAGS.checkpoint_dir)
-  if not os.path.exists(FLAGS.sample_dir):
-    os.makedirs(FLAGS.sample_dir)
+  checkpoint_dir = log_dir + "/checkpoints"
+  sample_dir = log_dir + "/samples"
+  if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+  if not os.path.exists(sample_dir):
+    os.makedirs(sample_dir)
 
-  #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-  run_config = tf.ConfigProto()
-  run_config.gpu_options.allow_growth=True
-
-  with tf.Session(config=run_config) as sess:
+  with tf.Session(master=server.target) as sess:
     if FLAGS.dataset == 'mnist':
       dcgan = DCGAN(
           sess,
@@ -81,7 +79,7 @@ def main(_):
     else:
       if not dcgan.load(FLAGS.checkpoint_dir)[0]:
         raise Exception("[!] Train a model first, then run test mode")
-      
+
 
     # to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
     #                 [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
@@ -92,6 +90,3 @@ def main(_):
     # Below is codes for visualization
     OPTION = 1
     visualize(sess, dcgan, FLAGS, OPTION)
-
-if __name__ == '__main__':
-  tf.app.run()
